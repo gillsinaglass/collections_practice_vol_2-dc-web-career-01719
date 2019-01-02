84add3d81d6587ba19325167f2bd604a6c5236e8
Ryan Gill <rdgill95@gmail.com>
2019-01-02 20:02:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,7 +41,7 @@ def organize_schools(schools)
   schools.each do |school, location|
     location.each do |key, value|
     if organized_schools[value] == nil
-      organized_schools[value] = school
+      organized_schools[value] = [school]
     else organized_schools[value] << school
       end
     end
